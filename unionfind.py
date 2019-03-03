ddc2fa1ddb995a97513576c9c5acdc895e66a12d
zekatsu <katsuyoso@yahoo.co.jp>
2019-03-03 10:57:15 +0900

    bug fix

--- unionfind.py
+++ unionfind.py
@@ -1,13 +1,13 @@
 class Unionfind():
     def __init__(self, size):
-        table = [-1 for _ in range(size)]
+        self.table = [-1 for _ in range(size)]
     def find(self, x):
         while(self.table[x] >= 0):
             x = self.table[x]
         return x
     def union(self, x, y):
-        xl = find(x)
-        yl = find(y)
+        xl = self.find(x)
+        yl = self.find(y)
         if xl != yl:
             if self.table[xl] <= self.table[yl]:
                 self.table[yl] = xl
