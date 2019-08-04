aa6564e4d6b159503ce481e1cf1a702b6b0fdf4a
zekatsu <katsuyoso@yahoo.co.jp>
2019-08-04 23:32:15 +0900

    add factorizaion

--- prime.py
+++ prime.py
@@ -1,4 +1,4 @@
-MAX_PRIME = 10**9
+MAX_PRIME = 10**5
 is_prime = [1] * MAX_PRIME
 primes = []
 is_prime[0] = 0
@@ -8,3 +8,18 @@ for i in range(MAX_PRIME):
         primes.append(i)
         for j in range(2, ((MAX_PRIME-1)//i) + 1):
             is_prime[i*j] = 0
+
+def factorization(n):
+    factor = {}
+    for p in primes:
+        if p ** 2 > n:
+            break
+        while n % p == 0:
+            n //= p
+            if p not in factor:
+                factor[p] = 1
+            else:
+                factor[p] += 1
+    if n > 1:
+        factor[n] = 1
+    return factor
