196cd8b8133a2e42a2f6ca726f0ad1c6d2d9f35d
zekatsu <katsuyoso@yahoo.co.jp>
2021-08-05 09:57:54 +0900

    class

--- prime.py
+++ prime.py
@@ -1,36 +1,46 @@
-MAX_PRIME = 10 ** 6
-is_prime = [1] * MAX_PRIME
-primes = []
-is_prime[0] = 0
-is_prime[1] = 0
-for i in range(MAX_PRIME):
-    if is_prime[i]:
-        primes.append(i)
-        for j in range(2, ((MAX_PRIME-1)//i) + 1):
-            is_prime[i*j] = 0
+class Primes:
 
-def factorization(n):
-    factor = {}
-    for p in primes:
-        if p ** 2 > n:
-            break
-        while n % p == 0:
-            n //= p
-            if p not in factor:
-                factor[p] = 1
-            else:
-                factor[p] += 1
-    if n > 1:
-        factor[n] = 1
-    return factor
+    def __init__(self, MAX_PRIME):
+        self.is_prime = [1] * MAX_PRIME
+        self.primes = []
+        self.is_prime[0] = 0
+        self.is_prime[1] = 0
+        for i in range(MAX_PRIME):
+            if self.is_prime[i]:
+                self.primes.append(i)
+                for j in range(2, ((MAX_PRIME-1)//i) + 1):
+                    self.is_prime[i*j] = 0
 
-n = int(input())
-f = factorization(n)
-k = list(f.keys())
-def dfs(num, i):
-    if i == len(k):
-        # 約数に対して何か行う
-    else:
-        p = k[i]
-        for j in range(f[p]+1):
-            ret += dfs(num * p ** j, i+1)
+    def factorization(self, n):
+        factor = {}
+        for p in self.primes:
+            if p ** 2 > n:
+                break
+            while n % p == 0:
+                n //= p
+                if p not in factor:
+                    factor[p] = 1
+                else:
+                    factor[p] += 1
+        if n > 1:
+            factor[n] = 1
+        return factor
+
+    def divisor(self, n):
+        div = {n}
+        fact = self.factorization(n)
+        for p in fact.keys():
+            new_div = div.copy()
+            for i in range(1, fact[p] + 1):
+                for d in div:
+                    assert((d % p ** i) == 0)
+                    new_div.add(d // p ** i)
+            div = new_div
+        return sorted(div)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    p = Primes(10 ** 6)
+    print(p.factorization(n))
+    print(p.divisor(n))
